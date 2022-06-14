0ec278f1e8dbeaf85033081456d049130849e139
rodolfopoly <rodolfopoly@gmail.com>
2022-06-14 19:04:57 -0400

    add the db

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INTEGER NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    PRIMARY KEY (id)
+);
