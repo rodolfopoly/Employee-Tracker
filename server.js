204eab48492ad3f8187fedc5cc883777ac4d5dd9
rodolfopoly <rodolfopoly@gmail.com>
2022-06-14 19:06:58 -0400

    importing package and doing the connection to the database

---
+++ server.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // {TODO: Add your MySQL password}
+        password: 'rootmysql',
+        database: 'employees_db'
+    });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`Connected to the employees_db database.`);
+    firstPrompt();
+})
